c545790446ed22a0d6d6a8f77edd4a10c742c9bc
Tagir <tagirbpl@gmail.com>
2021-09-03 14:06:10 +0300

    Add test for invalid answer & redirect

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -13,8 +13,20 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     context 'with valid attributes' do
+      let(:create_answer) { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }
       it 'saves a new answer into db' do
-        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(question.answers, :count).by 1
+        expect { create_answer }.to change(question.answers, :count).by 1
+      end
+
+      it 'redirects to question path' do
+        create_answer
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save a new answer' do
+        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer, :invalid_answer) } }.to change(question.answers, :count).by 0
       end
     end
   end
