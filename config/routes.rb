dac9ea0eb1413bae7c6def0d79a22a79a03f1c89
Tagir <tagirbpl@gmail.com>
2021-09-04 22:27:39 +0300

    Implement acceptance tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  
   resources :questions do
     resources :answers, shallow: true, only: %i[new create]
   end
+
+  root to: 'questions#index'
 end
