7701984cf1039a8f3c07fd504b6c91e1b6e4d7d2
Tagir <tagirbpl@gmail.com>
2021-09-25 14:29:49 +0300

    Fixes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -10,12 +10,21 @@ RSpec.describe Answer, type: :model do
     let!(:user)     { create :user }
     let!(:question) { create :question, best_answer_id: nil }
     let!(:answer)   { create :answer, question: question, user: user }
+    let!(:second_answer)   { create :answer, question: question, user: user }
 
     it '#mark_as_best' do
       question.update(best_answer_id: answer.id)
       question.reload
 
       expect(question).to have_attributes(best_answer_id: answer.id)
+    end
+
+    it '#best_answer?' do
+      question.update(best_answer_id: answer.id)
+      question.reload
+
+      expect(question.best_answer_id).to eq(answer.id)
+      expect(question.best_answer_id).not_to eq(second_answer.id)
     end
   end
 end
