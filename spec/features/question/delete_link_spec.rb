eb372cc35d2cabd16734b90cc9bae58d5a965330
Tagir <tagirbpl@gmail.com>
2021-10-10 21:46:25 +0300

    Implement add/delete links feature for Question

--- spec/features/question/delete_link_spec.rb
+++ spec/features/question/delete_link_spec.rb
@@ -3,16 +3,23 @@
 require 'rails_helper'
 
 feature 'User can delete links to question' do
- 	given(:user) { create :user }
-  given(:gist_url) { 'https://gist.github.com/tagirahmad/62598000f63a19949cbfeb39793a3c29' }
-  given(:gist_url2) { 'https://google.com' }
-  given(:question) { create :question, user: user  }
+ 	given(:user)      { create :user }
+  given(:question)  { create :question, user: user  }
+  given!(:link)     { create :link, linkable: question }
 
-  before { login user  }
+  context 'Owner' do 
+    scenario 'User deletes links when edit question', js: true do
+      login user
+      visit question_path question
 
-  scenario 'User deletes links when asks question', js: true do
+      save_and_open_page
 
+      within '.links' do
+        click_on 'Delete'
+      end
+
+      expect(page).not_to have_link link.name, href: link.url
+    end
   end
-
 end
 
