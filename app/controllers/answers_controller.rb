b050fc5fc7831faad1e311ef41e60c9fd7a3153f
Tagir <tagirbpl@gmail.com>
2021-10-12 21:39:07 +0300

    add link tests for answer & question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -46,6 +46,6 @@ class AnswersController < ApplicationController
   end
 
   def answer_params
-    params.require(:answer).permit(:title, files: [], links_attributes: %i[name url])
+    params.require(:answer).permit(:title, files: [], links_attributes: %i[name url id])
   end
 end
