a41105b6cfa0a32cc2c95a1c63ee9f2f19fc46e7
Tagir <tagirbpl@gmail.com>
2021-10-12 20:41:27 +0300

    add validations to Link

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,8 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
   
   validates :name, :url, presence: true
+  validates_format_of :url,
+                      with: %r{(http|https)://[a-zA-Z0-9\-\#/\_]+[\.][a-zA-Z0-9\-\.\#/\_]+}i,
+                      on: :create,
+                      message: 'please enter in correct format'
 end
