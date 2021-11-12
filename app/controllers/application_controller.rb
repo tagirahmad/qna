baf054b9160fa99aa844b621075131f372a7f97f
Tagir Ahamadeev <tagirbpl@gmail.com>
2021-11-12 21:16:49 +0300

    fix authorizations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  respond_to :html, :json, :js
+  
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    respond_to do |format|
+      format.html { redirect_to root_url, alert: exception.message }
+      format.json { render json: { response: exception.message }, status: 403 }
+      format.js   { render json: { response: exception.message }, status: 403 }
+    end
   end
 
-  check_authorization unless: :devise_controller?
+ # check_authorization unless: :devise_controller?
 end
+
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -26,14 +26,18 @@ class Ability
   def user_abilities
     guest_abilities
 
-    can :create, [Question, Answer, Comment]
-    can :update, [Question, Answer, Comment],  user: user
-    can :destroy, [Question, Answer, Comment], user: user
+    can :create,  [Question, Answer, Comment]
+    can :update,  [Question, Answer, Comment], user_id: user.id
+    can :destroy, [Question, Answer, Comment], user_id: user.id
 
     can :mark_as_best, Answer, question: { user_id: user.id }
 
     can %i[vote_up vote_down unvote], [Question, Answer] do |resource|
-      resource.user_id != user.id
+      not user.author_of?(resource)
+    end
+
+    can :unvote, [Question, Answer] do |resource|
+      user.author_of?(resource)
     end
 
     can :destroy, Link, linkable: { user_id: user.id }
