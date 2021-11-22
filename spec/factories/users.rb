398ec3d4ab7b4cc357f1779b46604d538fb54bac
Tagir <tagirbpl@gmail.com>
2021-11-22 17:01:11 +0300

    user factory fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
   end
 
   factory :user do
-    email                 { 'email@mail.com' }
+    email
     password              { '12345678' }
     password_confirmation { '12345678' }
   end
