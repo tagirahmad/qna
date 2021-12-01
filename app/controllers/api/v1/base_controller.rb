939369ed971ccbf2724594a9c508c7acb048ac0a
Tagir <tagirbpl@gmail.com>
2021-12-01 20:28:48 +0300

    fix

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -7,11 +7,7 @@ module Api
       private
 
       def current_user
-        if doorkeeper_token
-          @current_user || User.find(doorkeeper_token.resource_owner_id)
-        else
-          warden.authenticate(scope: :user)
-        end
+        @current_user ||= User.find(doorkeeper_token.resource_owner_id)
       end
     end
   end
